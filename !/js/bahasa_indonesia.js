4767d21302f730483b7a318aab9c4d1e68259eb0
SAMBUYYA <83904969+SAMBUYYA@users.noreply.github.com>
2022-12-23 16:59:35 +0700

    Rename bahasa_indonesia.js to !/js/bahasa_indonesia.js

---
+++ !/js/bahasa_indonesia.js
@@ -0,0 +1,1 @@
+function bahasa_indonesia(){return"indonesia"==localStorage.getItem("bahasa_saat_ini")||"inggris"!=localStorage.getItem("bahasa_saat_ini")&&("id"==navigator.language||"id-ID"==navigator.language)}
--- bahasa_indonesia.js
+++
@@ -1,1 +0,0 @@
-function bahasa_indonesia(){return"indonesia"==localStorage.getItem("bahasa_saat_ini")||"inggris"!=localStorage.getItem("bahasa_saat_ini")&&("id"==navigator.language||"id-ID"==navigator.language)}
